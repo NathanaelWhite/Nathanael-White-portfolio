42994fc2e4b9a7342783082d95d2a1ebe746dddd
Nathanael White <narhanaelw99@gmail.com>
2021-09-18 11:35:10 -0500

    editing navbar with bootstrap

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,21 +1,25 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Navigation() {
-    const tabs = ["Home", "About", "Work", "Contact"];
-    return (
-        <>
-        <Nav className="justify-content-center" activeKey="/home">
-            {tabs.map(tab => (
-                <Nav.item>
-                    <Nav.link href="#" eventKey={tab}>{tab}</Nav.link>
-                </Nav.item> 
-            ))}
-        </Nav>
-        </>
-    )
+  const tabs = ["Home", "About", "Work", "Contact"];
+  return (
+    <Navbar bg="light" expand="lg">
+      <container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </container>
+    </Navbar>
+  );
 }
 
 export default Navigation;
