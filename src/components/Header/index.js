bbe9da0d4a920ee0ca57e14ca73432736ce73683
Nathanael White <narhanaelw99@gmail.com>
2021-09-20 21:17:19 -0500

    cleaned up header component and started styling on about component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,12 +4,9 @@ import video from "./video.mp4";
 
 function Header() {
   return (
-    <div>
       <video autoPlay loop muted id="bg-video">
         <source src={video} type="video/mp4" />
-      <h1 className="header-title">Nathanael White</h1>
       </video>
-    </div>
   );
 }
 
